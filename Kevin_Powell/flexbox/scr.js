3cef308b9a18a95b16fd11109448658cd173ab99
Vitalii Marchenko <vitaly.marchenko77@gmail.com>
2022-02-11 07:04:35 +0300

    vue example ends

--- Kevin_Powell/flexbox/scr.js
+++ Kevin_Powell/flexbox/scr.js
@@ -1,37 +1,56 @@
 var app = new Vue({
   el: '#app',
   data: {
-      form: {
-          name: '',
-          email: '',
-          message: '',
-      },
-      error: '',
+    form: {
+      name: '',
+      email: '',
+      message: '',
+    },
+    error: '',
+    // field: '',
+    firstName: '',
+    lastName: '',
   },
   methods: {
-      validateForm: function () {
-          if (this.form.name.length && this.form.email.length && this.form.message.length) {
-              return this.error = 'Form Sent!';
-          }
-
-          return this.error = 'Invalid Form';
+    validateForm: function () {
+      if (
+        this.form.name.length &&
+        this.form.email.length &&
+        this.form.message.length
+      ) {
+        return (this.error = 'Form Sent!');
       }
+
+      return (this.error = 'Invalid Form');
+    },
   },
   computed: {
-      nameValidation: function () {
-          if ( ! this.form.name.length && this.error) {
-              return 'Name Field is Required';
-          }
+    fullName: {
+      get: function () {
+        console.log(this.firstName);
+        return this.firstName + ' ' + this.lastName;
       },
-      emailValidation: function () {
-          if ( ! this.form.email.length && this.error) {
-              return 'Name Field is Required';
-          }
+      set: function (newValue) {
+        var names = newValue.split(' ');
+
+        this.firstName = names[0];
+        this.lastName = names[1];
       },
-      messageValidation: function () {
-          if ( ! this.form.message.length && this.error) {
-              return 'Name Field is Required';
-          }
-      },
-  }
+    },
+    nameValidation: function () {
+      if (!this.form.name.length && this.error) {
+        return 'Name Field is Required';
+      }
+    },
+    emailValidation: function () {
+      if (!this.form.email.length && this.error) {
+        return 'Name Field is Required';
+      }
+    },
+    messageValidation: function () {
+      if (!this.form.message.length && this.error) {
+        return 'Name Field is Required';
+      }
+    },
+  },
 });
