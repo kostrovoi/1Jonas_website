b49db7fef7d440bd5da07ebc636924c1ed8b1df0
Vitalii Marchenko <vitaly.marchenko77@gmail.com>
2022-02-13 23:20:22 +0300

    add smooth scrolling animation on js

--- 07-Omnifood-Desktop/js/index.js
+++ 07-Omnifood-Desktop/js/index.js
@@ -33,3 +33,35 @@ console.log(mobileNavEl);
 mobileNavEl.addEventListener('click', () => {
   headerEl.classList.toggle('nav-open');
 })
+
+// smooth scrolling animation
+const allLinks = document.querySelectorAll('a:link');
+console.log(allLinks);
+
+allLinks.forEach(function(link) {
+  link.addEventListener('click', (event) => {
+    console.log(event);
+    event.preventDefault();
+    const href = link.getAttribute('href');
+    console.log(href);
+
+    // scroll back to top
+    if(href === '#') window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+
+    // scroll to id selector
+    if(href !== '#' && href.startsWith('#')) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({behavior: 'smooth'});
+    }
+
+    // close mobile navigation
+    if(link.classList.contains('main-nav-link')) {
+      headerEl.classList.toggle('nav-open');
+    }
+  });
+});
+
