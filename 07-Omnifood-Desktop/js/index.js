8f8ea7edf1055ad7e9343c3f842c273707fb2d9f
Vitalii Marchenko <vitaly.marchenko77@gmail.com>
2022-02-15 22:54:34 +0300

    Fixing flexbox gap property with js & css

--- 07-Omnifood-Desktop/js/index.js
+++ 07-Omnifood-Desktop/js/index.js
@@ -94,3 +94,23 @@ const observer = new IntersectionObserver(function(entries) {
   // '-80px' === 8rem the height of sticky header
 });
 observer.observe(sectionHeroEl);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
